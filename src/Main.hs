d35d8c4b9742609014455e19a4749f8f0c05086e
Artyom <yom@artyom.me>
2015-08-25 18:46:20 +0300

    Use Sqlite instead of text database

--- src/Main.hs
+++ src/Main.hs
@@ -10,9 +10,10 @@ module Main where
 -- General
 import Control.Monad
 import Data.Foldable
+import Data.Traversable
 import Data.Maybe
-import Data.Functor.Identity
 import Control.Monad.State
+import Data.Int
 -- Lenses
 import Lens.Micro
 -- Lists
@@ -20,24 +21,23 @@ import Data.List (uncons)
 -- Command-line options
 import Options.Applicative
 -- IO
-import System.IO
+import System.Directory
 -- Text
-import Data.Char
 import Text.Read (readMaybe)
 import Text.Printf
 import qualified Data.Text as T
-import qualified Data.Text.IO as T
 import Data.Text (Text)
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.IO as TL
 -- Containers
 import qualified Data.HashMap.Strict as HM
 import Data.HashMap.Strict (HashMap)
-import qualified Data.Vector as V
-import Data.Vector (Vector)
 import qualified Data.HashSet as HS
 -- Hashable
 import Data.Hashable
+-- SQL
+import qualified Database.SQLite.Simple as SQL
+import Database.SQLite.Simple (Only(..))
 
 
 data Command
@@ -59,69 +59,74 @@ cmdargs = info
     countDesc = "count total plays of all songs by an artist"
     artistP = T.pack <$> strArgument (metavar "ARTIST")
 
-type UserId   = Int
-type SongId   = Text
-type ArtistId = Int
-type Artist   = Text
-type Title    = Text
-
-data Listen = Listen {
-  user   :: UserId,
-  song   :: SongId,
-  artist :: ArtistId,
-  plays  :: Int }
-
-data DataFile = DataFile {
-  dataArtists   :: Vector Artist,
-  dataArtistIds :: HashMap Artist ArtistId,
-  dataSongs     :: HashMap SongId (ArtistId, Title),
-  dataListens   :: [Listen] }
-
-{- |
-The file is stored as follows:
-
-* amount of artists
-* artists
-* amount of songs
-* (song, artist, title) tuples
-* the rest is listens data
-
-See 'buildFile' for details.
--}
-readData :: TL.Text -> DataFile
-readData s = flip evalState (strictLines s) $ do
-  let uncons' (x:xs) = (x, xs)  
-  -- Artists:
-  numArtists <- tread <$> state uncons'
-  artists    <- state (splitAt numArtists)
-  let dataArtists   = V.fromList artists
-      dataArtistIds = HM.fromList (zip artists [0..])
-  -- Songs:
-  numSongs <- tread <$> state uncons'
-  songs    <- state (splitAt numSongs)
-  let parseSong s = do
-        [song, artist, title] <- return (T.splitOn "\t" s)
-        return (song, (tread artist, title))
-      dataSongs = HM.fromList $ mapMaybe parseSong songs
-  -- Listens:
-  listens <- get
-  let parseListen s = do
-        [user, song, artist, plays] <- return (T.splitOn "\t" s)
-        return Listen{
-          user   = tread user,
-          song   = song,
-          artist = tread artist,
-          plays  = tread plays }
-      dataListens = mapMaybe parseListen listens
-  
-  return DataFile{..}
+type UserId     = Int64
+type SongTextId = Text
+type SongId     = Int64
+type ArtistId   = Int64
+type Artist     = Text
+type Title      = Text
 
 main :: IO ()
 main = do
   cmd <- execParser cmdargs
   case cmd of
-    Build -> buildFile
+    Build -> buildMusicDb
     Count{..} -> countPlays cmdArtist
+
+-- Read track data (from unique_tracks.txt).
+readTracks :: TL.Text -> HashMap SongTextId (Artist, Title)
+readTracks = HM.fromList . mapMaybe parseTrack . strictLines
+  where
+    parseTrack s = do
+      [_,song, artist, title] <- return (T.splitOn "<SEP>" s)
+      return (song, (artist, title))
+
+-- Read listens data (from train_triplets.txt).
+readListens :: TL.Text -> [(Text, SongTextId, Int)]
+readListens = mapMaybe parseListen . strictLines
+  where
+    parseListen s = do
+      [user, song, playsText] <- return (T.splitOn "\t" s)
+      plays <- readMaybe (T.unpack playsText)
+      return (user, song, plays)
+
+initMusicDb :: SQL.Connection -> IO ()
+initMusicDb db = do
+  -- A table for artists:
+  SQL.execute_ db
+    "CREATE TABLE artists (\
+    \id INTEGER PRIMARY KEY,\
+    \name TEXT NOT NULL UNIQUE)"
+  SQL.execute_ db
+    "CREATE INDEX index_artists_by_name ON artists (name)"
+  -- A table for songs:
+  SQL.execute_ db
+    "CREATE TABLE songs (\
+    \id INTEGER PRIMARY KEY,\
+    \text_id TEXT NOT NULL,\
+    \artist INTEGER NOT NULL,\
+    \title TEXT NOT NULL,\
+    \FOREIGN KEY(artist) REFERENCES artists(id))"
+  SQL.execute_ db
+    "CREATE INDEX index_songs_by_artist ON songs (artist)"
+  -- A table for listens:
+  SQL.execute_ db
+    "CREATE TABLE listens (\
+    \user INTEGER NOT NULL,\
+    \song INTEGER NOT NULL,\
+    \plays INTEGER NOT NULL,\
+    \FOREIGN KEY(song) REFERENCES songs(id),\
+    \UNIQUE (user, song))"
+  SQL.execute_ db
+    "CREATE INDEX index_listens_by_song ON listens (song)"
+
+withMusicDb :: (SQL.Connection -> IO a) -> IO a
+withMusicDb act = do
+  db <- SQL.open "music.db"
+  SQL.execute_ db "PRAGMA foreign_keys = ON"
+  result <- act db
+  SQL.close db
+  return result
 
 {- |
 Build a data file out of unique_tracks.txt and train_triplets.txt.
@@ -132,81 +137,72 @@ Links to the dataset:
 
   * song titles: <http://labrosa.ee.columbia.edu/millionsong/sites/default/files/AdditionalFiles/unique_tracks.txt>
 -}
-buildFile :: IO ()
-buildFile = do
-  -- First read track data from unique_tracks.txt.
-  let readTracks :: TL.Text -> HashMap SongId (Artist, Title)
-      readTracks = HM.fromList . mapMaybe parseTrack . strictLines
-      parseTrack s = do
-        [_,song, artist, title] <- return (T.splitOn "<SEP>" s)
-        return (song, (artist, title))
+buildMusicDb :: IO ()
+buildMusicDb = do
+  -- Read track data.
   songsIndex <- readTracks <$> TL.readFile "unique_tracks.txt"
-  
-  -- Then read listens data from train_triplers.txt.
-  let readListens :: TL.Text -> [(Text, SongId, Int)]
-      readListens = mapMaybe parseListen . strictLines
-      parseListen s = do
-        [user, song, playsText] <- return (T.splitOn "\t" s)
-        plays <- readMaybe (T.unpack playsText)
-        return (user, song, plays)
-  -- The listens file is big and we don't want to store it in memory, so
-  -- we're going to use getListens whenever we actually need listens.
-  let getListens = readListens <$> TL.readFile "train_triplets.txt"
-
-  -- Then prepare the data.
-  -- We need to have a list of artists:
   let artists = hashNub . map fst . HM.elems $ songsIndex
-  -- A reverse index for artists (to be able to quickly match an artist to
-  -- its index):
-  let artistsIndex = HM.fromList (zip artists [0..])
-  -- A reverse index for users (because numerical ids are better than guids
-  -- that are used in the original file):
-  usersIndex <- HM.fromList . indexConsecutive . map (^. _1) <$> getListens
-  -- Songs index using artist ids instead of artists:
-  let songsIndex' = songsIndex & traversed . _1 %~ (artistsIndex HM.!)
-  
-  -- Listens:
-  listens <- getListens
-
-  -- File writing begins.
-  T.writeFile "listens.txt" ""
-  let write = TL.writeFile "listens.txt" . TL.unlines . map TL.fromStrict
-  -- Amount of artists:
-  let artistCount = length artists
-  write [tshow artistCount]
-  printf "artists: %d\n" artistCount
-  -- Artists:
-  write artists
-  printf "done writing artists\n"
-  -- Amount of songs:
-  let songCount = HM.size songsIndex'
-  write [tshow songCount]
-  printf "songs: %d\n" songCount
-  -- (song, artist, title) tuples:
-  write $ flip map (HM.toList songsIndex') $ \(song, (artist, title)) ->
-    T.intercalate "\t" [
-      song,
-      tshow artist,
-      title ]
-  printf "done writing songs\n"
-  -- (user, song, artist, plays) tuples:
-  let userCount = HM.size usersIndex
-  printf "users: %d\n" userCount
-  write $ flip map listens $ \(user, song, plays) ->
-    T.intercalate "\t" [
-      tshow (usersIndex HM.! user),
-      song,
-      tshow (fst (songsIndex' HM.! song)),
-      tshow plays ]
-  printf "done writing listens\n"
+  -- Read listens data.
+  listens <- readListens <$> TL.readFile "train_triplets.txt"
+  -- Write stuff to the database.
+  exists <- doesFileExist "music.db"
+  when exists $ removeFile "music.db"
+  withMusicDb $ \db -> do
+    initMusicDb db
+    -- Trade some safety for speed.
+    SQL.execute_ db "PRAGMA synchronous = OFF"
+    SQL.execute_ db "PRAGMA journal_mode = MEMORY"
+    -- Artists:
+    SQL.withTransaction db $
+      mapM_ (writeArtist db) artists
+    putStrLn "done writing artists"
+    -- Songs:
+    songIds <- fmap HM.fromList $ SQL.withTransaction db $
+      for (HM.toList songsIndex) $ \(song, (artist, title)) ->
+        writeSong db song artist title
+    putStrLn "done writing songs"
+    -- Listens:
+    let listens' = listens & each . _2 %~ (songIds HM.!)
+    SQL.withTransaction db $
+      foldM_ (writeListen db) ("fake user identificator", 0::Int) listens'
+    putStrLn "done writing listens"
+  where
+    writeArtist db artist = SQL.execute db
+      "INSERT INTO artists (name) VALUES (?)"
+      (Only artist)
+    writeSong db song artist title = do
+      SQL.execute db
+        "INSERT INTO songs (text_id, artist, title) \
+        \VALUES (?, (SELECT id FROM artists WHERE name = ?), ?)"
+        (song, artist, title)
+      songId <- SQL.lastInsertRowId db
+      return (song, songId)
+    writeListen db (prevUser, prevUserId) (user, songId, plays) = do
+      let userId | user == prevUser = prevUserId
+                 | otherwise        = prevUserId + 1
+      SQL.execute db
+        "INSERT INTO listens (user, song, plays) \
+        \VALUES (?, ?, ?)"
+        (userId, songId, plays)
+      return (user, userId)
 
 countPlays :: Artist -> IO ()
-countPlays cmdArtist = do
-  DataFile{..} <- readData <$> TL.readFile "listens.txt"
-  let artistId = dataArtistIds HM.! cmdArtist
-  let total = sum . map plays . filter (\x -> artist x == artistId)
-            $ dataListens
-  print total
+countPlays cmdArtist = withMusicDb $ \db -> do
+  mbArtist <- SQL.query db
+    "SELECT id FROM artists WHERE name = ?"
+    (Only cmdArtist)
+  artist <- case mbArtist of
+    [Only x] -> return (x :: ArtistId)
+    []       -> error "no artists with this name"
+    _        -> error "several artists with this name"
+  [Only res] <- SQL.query db
+    "SELECT sum(listens.plays) \
+    \FROM listens \
+    \INNER JOIN (SELECT * FROM songs WHERE artist=?) AS songs_by_artist \
+    \ON listens.song = songs_by_artist.id"
+    (Only artist)
+  print (res :: Int)
+
 
 -- Utils
 
@@ -215,12 +211,6 @@ tshow = T.pack . show
 
 tread :: Read a => Text -> a
 tread = read . T.unpack
-
-indexConsecutive :: Eq a => [a] -> [(a, Int)]
-indexConsecutive = go 0
-  where
-    go i []     = []
-    go i (x:xs) = (x, i) : go (i+1) (dropWhile (== x) xs)
 
 hashNub :: (Eq a, Hashable a) => [a] -> [a]
 hashNub = HS.toList . HS.fromList
